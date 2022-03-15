b0d5debce4b26d3dae21790ca80c112ceffe0014
nikita <Nikita12312123@gmail.com>
2022-03-15 21:31:26 +0200

    2nd commit (dev branch)

--- lesson4/src/Main.java
+++ lesson4/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("this was in my second commit, nothing more");
+        System.out.println("this was in branch named \"develop\"");
     }
 }
